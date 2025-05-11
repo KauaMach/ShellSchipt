5004b6293bb62bd6e7287fffd76665c1039dd123
KauaMach <kaua.silva@ufpi.edu.br>
2025-05-10 21:27:56 -0300

    ...

---
+++ Aulas.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+#time -> tempo de execução do comando
+#seq 1 10 -> gera uma sequência de números de 1 a 10
+#for i in {1..10}; do echo "$i"; done -> loop que imprime os números de 1 a 10
+# time for i in {1..1000}; do echo "$i"; done -> tempo de execução do loop que imprime os números de 1 a 1000 
+#echo $SHELL -> imprime o shell atual
+#echo $0 -> imprime o nome do script atual
+#Which -> verifica se o comando está instalado
+#type -p -> verifica se o comando está instalado
+
+# -----------------------------------
+#!/bin/bash 
+echo "Digite seu nome: "
+read nome
+echo "Olá, $nome!"
+#------------------------------------
+
+# o que é o whiptail?
+# o whiptail é um programa que permite criar caixas de diálogo em scripts de shell
+# ele é usado para criar interfaces gráficas simples em scripts de shell
+# --msgbox é uma opção do whiptail que cria uma caixa de mensagem
+whiptail --msgbox "oi" 0 0 
+
+#------------------------------------
---
+++ Hello.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+echo -e "\033[31;1mHello, \033[32;1mWorld!\033[0m"
